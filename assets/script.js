b595403862a98341fee4a58e4ebc3b76da780f6f
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-01-24 14:54:28 -0500

    more java

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var searchHistoryArr = [];
 var userInput = "";
 var APIKey = "3758324bb7cc715bc0076675d23131b9";
 
+
 $('#submitBtn').click(function(event) {
     event.preventDefault();
     var userInput = $("#input").val();
@@ -18,6 +19,33 @@ $('#submitBtn').click(function(event) {
     }) 
     .then(function (response) {
         console.log(response);
+
+    //Transfer content to HTML
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    // var now = moment().format('L');
+    $("#cityNameResult").text(response.name);
+    // $("#dayIcon").text(response.weather.[i].main);
+    // $("#now").text(now);
+    $("#temp").text("Temp: " + tempF.toFixed(1) + "⁰ F");
+    $("#humid").text("Humidity: " + response.main.humidity + "%");
+    $("#wind").text("Wind: " + response.wind.speed + " MPH");
+    $("#UV").text("UV index: ");
+    var UV = 65;
+    var btn = $("<button>");
+    btn.addClass("UVBtn");
+
+    if (UV < 70) {
+        $(".UVBtn").addClass("yellow");
+    }
+    // else if (UV > 89) {
+    //     $(".UVBtn").addClass("red");
+    // }
+    // else {
+    //     $(".UVBtn").addClass("orange");
+    // }
+    
+    btn.text(UV);
+    $("#UV").append(btn);
     })
 })
 
