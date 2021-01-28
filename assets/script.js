292f4da84a26d824d8f741fc4c49f162da9b3ba9
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-01-28 11:15:49 -0500

    icons

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ function pullLS() {
     }
     renderDash();
 }
-console.log(searchHistoryArr);
+
 pullLS();
 
 $('#clearBtn').click(function(event) {
@@ -38,7 +38,9 @@ $('#submitBtn').click(function(event) {
     $("#listHistory").prepend(userInput);
     saveLS();
     $("#dayIcon").removeClass();
+    $(".colFive").remove();
     $("#dayIcon").addClass("col-6 dayIcon");
+ 
 
     //Establish URL for call #1 to acquire lon & lat values based on city name
     var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + APIKey;
@@ -109,25 +111,17 @@ $('#submitBtn').click(function(event) {
         }
 
     //Five day forecast
-    // fiveDayArr = [];
-    //     console.log(response.daily.length);
   
     for (var i = 1; i < 6; i++) {
         var fiveDayDate = new Date(response.daily[i].dt*1000).toLocaleDateString();
         var fiveDayIcon = response.daily[i].weather[0].main;
         var fiveDayTemp = (((response.daily[i].temp.max - 273.15) * 1.80 + 32).toFixed(1) + "⁰ F");
         var fiveDayHumid = (response.daily[i].humidity + "%");
-        // fiveDayArr.push(fiveDayDate);
-        // fiveDayArr.push(fiveDayIcon);
-        // fiveDayArr.push(fiveDayTemp);
-        // fiveDayArr.push(fiveDayHumid);
-        console.log(fiveDayDate);
-        console.log(fiveDayIcon);
-        console.log(fiveDayTemp);
-        console.log(fiveDayHumid);
-        var col = $("<div>").addClass("col-2 col d-flex flex-column");
         
-        if (fiveDayIcon === "Rain") {col.addClass("fas fa-cloud-rain");}
+        var col = $("<div>").addClass("col-2 col colFive d-flex flex-column");
+
+        if (fiveDayIcon === "Rain") {col.addClass("fas fa-cloud-rain");
+        }
     
         else if (fiveDayIcon === "Snow") {col.addClass("fas fa-snowman");
         }
@@ -152,155 +146,6 @@ $('#submitBtn').click(function(event) {
         $(".fiveDayRow").append(col);
     }
    
-
-    // Day 1 of 5 /////////////////////////////////////////
-    $(".dayOne").removeClass();
-    $("#dayOne").addClass("col-6 dayIcon");
-    $("#dayOneDt").text(Date(response.daily[0].dt));
-    console.log(new Date(response.daily[0].dt*1000).toLocaleDateString());
-    console.log(new Date(response.daily[1].dt*1000).toLocaleDateString());
-    var dayOneIconText = response.daily[0].weather[0].main;
-    console.log(dayOneIconText);
-    
-    if (dayOneIconText === "Rain") {$(".dayOne").addClass("fas fa-cloud-rain");
-        }
-    
-        else if (dayOneIconText === "Snow") {$(".dayOne").addClass("fas fa-snowman");
-        }
-        
-        else if (dayOneIconText === "Wind") {$(".dayOne").addClass("fas fa-wind");
-        }
-    
-        else if (dayOneIconText === "Sun") {$(".dayOne").addClass("fas fa-sun");
-        }
-    
-        else if (dayOneIconText === "Clear") {$(".dayOne").addClass("fas fa-moon");
-        }
-    
-        else if (dayOneIconText === "Clouds") {$(".dayOne").addClass("fas fa-cloud");
-        }
-    
-        else {$(".dayOne").addClass("fas fa-rainbow");
-        }
-    
-    $("#dayOneTemp").text(((response.daily[0].temp.max - 273.15) * 1.80 + 32).toFixed(1) + "⁰ F");
-    $("#dayOneHumid").text(response.daily[0].humidity + "% humidity");
-
-    // Day 2 of 5 /////////////////////////////////////////
-    $(".dayTwo").removeClass();
-    $("#dayTwo").addClass("col-6 dayIcon");
-    $("#dayTwoDt").text(Date(response.daily[1].dt));
-    var dayTwoIconText = response.daily[1].weather[1].main;
-    console.log(dayTwoIconText);
-    if (dayTwoIconText === "Rain") {$(".dayTwo").addClass("fas fa-cloud-rain");
-        }
-    
-        else if (dayTwoIconText === "Snow") {$(".dayTwo").addClass("fas fa-snowman");
-        }
-        
-        else if (dayTwoIconText === "Wind") {$(".dayTwo").addClass("fas fa-wind");
-        }
-    
-        else if (dayTwoIconText === "Sun") {$(".dayTwo").addClass("fas fa-sun");
-        }
-    
-        else if (dayTwoIconText === "Clear") {$(".dayTwo").addClass("fas fa-moon");
-        }
-    
-        else if (dayTwoIconText === "Clouds") {$(".dayTwo").addClass("fas fa-cloud");
-        }
-    
-        else {$(".dayOne").addClass("fas fa-rainbow");
-        }
-    
-    $("#dayTwoTemp").text(((response.daily[1].temp.max - 273.15) * 1.80 + 32).toFixed(1) + "⁰ F");
-    $("#dayTwoHumid").text(response.daily[1].humidity + "% humidity");
-
-     // Day 3 of 5 /////////////////////////////////////////
-     $(".dayThree").removeClass();
-    $("#dayT").addClass("col-6 dayIcon");
-     $("#dayThreeDt").text(Date(response.daily[2].dt));
-     var dayThreeIconText = response.current.weather[1].main;
-     console.log(dayThreeIconText);
-     if (dayThreeIconText === "Rain") {$(".dayThree").addClass("fas fa-cloud-rain");
-         }
-     
-         else if (dayThreeIconText === "Snow") {$(".dayThree").addClass("fas fa-snowman");
-         }
-         
-         else if (dayThreeIconText === "Wind") {$(".dayThree").addClass("fas fa-wind");
-         }
-     
-         else if (dayThreeIconText === "Sun") {$(".dayThree").addClass("fas fa-sun");
-         }
-     
-         else if (dayThreeIconText === "Clear") {$(".dayThree").addClass("fas fa-moon");
-         }
-     
-         else if (dayThreeIconText === "Clouds") {$(".dayThree").addClass("fas fa-cloud");
-         }
-     
-         else {$(".dayThree").addClass("fas fa-rainbow");
-         }
-     
-     $("#dayThreeTemp").text(((response.daily[2].temp.max - 273.15) * 1.80 + 32).toFixed(1) + "⁰ F");
-     $("#dayThreeHumid").text(response.daily[2].humidity + "% humidity");
-    
-     // Day 4 of 5 /////////////////////////////////////////
-     $("#dayFourDt").text(Date(response.daily[3].dt));
-     // var dayOneIconText = response.current.weather[1].main;
-     // console.log(dayOneIconText);
-     // if (dayOneIconText === "Rain") {$(".dayOne").addClass("fas fa-cloud-rain");
-     //     }
-     
-     //     else if (dayOneIconText === "Snow") {$(".dayOne").addClass("fas fa-snowman");
-     //     }
-         
-     //     else if (dayOneIconText === "Wind") {$(".dayOne").addClass("fas fa-wind");
-     //     }
-     
-     //     else if (dayOneIconText === "Sun") {$(".dayOne").addClass("fas fa-sun");
-     //     }
-     
-     //     else if (dayOneIconText === "Clear") {$(".dayOne").addClass("fas fa-moon");
-     //     }
-     
-     //     else if (dayOneIconText === "Clouds") {$(".dayOne").addClass("fas fa-cloud");
-     //     }
-     
-     //     else {$(".dayOne").addClass("fas fa-rainbow");
-     //     }
-     
-     $("#dayFourTemp").text(((response.daily[3].temp.max - 273.15) * 1.80 + 32).toFixed(1) + "⁰ F");
-     $("#dayFourHumid").text(response.daily[3].humidity + "% humidity");
-
-     // Day 5 of 5 /////////////////////////////////////////
-     $("#dayFiveDt").text(Date(response.daily[4].dt));
-     // var dayOneIconText = response.current.weather[1].main;
-     // console.log(dayOneIconText);
-     // if (dayOneIconText === "Rain") {$(".dayOne").addClass("fas fa-cloud-rain");
-     //     }
-     
-     //     else if (dayOneIconText === "Snow") {$(".dayOne").addClass("fas fa-snowman");
-     //     }
-         
-     //     else if (dayOneIconText === "Wind") {$(".dayOne").addClass("fas fa-wind");
-     //     }
-     
-     //     else if (dayOneIconText === "Sun") {$(".dayOne").addClass("fas fa-sun");
-     //     }
-     
-     //     else if (dayOneIconText === "Clear") {$(".dayOne").addClass("fas fa-moon");
-     //     }
-     
-     //     else if (dayOneIconText === "Clouds") {$(".dayOne").addClass("fas fa-cloud");
-     //     }
-     
-     //     else {$(".dayOne").addClass("fas fa-rainbow");
-     //     }
-     
-     $("#dayFiveTemp").text(((response.daily[4].temp.max - 273.15) * 1.80 + 32).toFixed(1) + "⁰ F");
-     $("#dayFiveHumid").text(response.daily[4].humidity + "% humidity");
         //second call function///////////////////////////////
     })
         //first call function///////////////////////////////////
